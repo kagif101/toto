ef9e2bc1bd024eb939c2e91d4309e2428a74c2e1
kagif101 <otnielsre@gmail.com>
2018-12-07 20:14:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3_eutOFasCoepO8aHqxinm3zY-mm2TAeHaakXq_lH-de5iuqpGys9uUl62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "a485ce30fcac6952";
+$os="android";
+$sign="3cf7a3f39428a590c1fe8189c2d846eb3018d5eedbb5162490fb5e6b9c2be798";
